07ea38b653eabc1a32916dfc9f73dd6b13d4c2e9
prateekagrawaliiit <prateekagrawaliiit@gmail.com>
2022-01-25 23:36:47 +0530

    Initial Commit Added Lab 1,2 and 4

---
+++ Lab 2/lab2_sysconfig.c
@@ -0,0 +1,26 @@
+/*
+* @Author: prateek
+* @Date:   2022-01-23 16:56:53
+* @Last Modified by:   prateek
+* @Last Modified time: 2022-01-23 16:57:00
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<sys/utsname.h>
+int main()
+{
+    struct utsname buf1;
+    errno = 0;
+    if (uname(&buf1) != 0)
+    {
+        perror("uname doesn't return 0, so there is an error");
+        exit(EXIT_FAILURE);
+    }
+    printf("System Name = %s\n", buf1.sysname);
+    printf("Node Name = %s\n", buf1.nodename);
+    printf("Version = %s\n", buf1.version);
+    printf("Release = %s\n", buf1.release);
+    printf("Machine = %s\n", buf1.machine);
+    int r = system("cat /proc/1/status");
+}
